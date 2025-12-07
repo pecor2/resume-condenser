d52abf4479c012e9c54313e831f132ba5cd64596
pecor2 <pecor2@illinois.edu>
2025-12-07 08:15:36 -0800

    standardize steps

--- demo.py
+++ demo.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     # Uncomment to parse resumes - This can take a long time so use the provided JSONL in the next step
     # parse_resume(path="resumes/", output="parsed_resumes.jsonl")
     
-    # Step 3: Index the JSONL documents into Elasticsearch
+    # Index the JSONL documents into Elasticsearch
     jsonl_to_es(jsonl_path="parser_output.jsonl", index_name="resume_index")
 
-    # Step 4: Run the interactive CLI for querying
+    # Run the interactive CLI for querying
     interactive_cli()
